8b23e710dd7b72d4f97cb556979480af2ce957d5
elena tarasova <nafanichka.ev@gmail.com>
2022-09-23 00:24:43 -0400

    Exersize3

---
+++ Exersize3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Write a number N: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Your numbers are: ");
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i*i*i}");
+}
