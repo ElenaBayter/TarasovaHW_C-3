9a2001d961b56e900eb6542b94cd59d176cb5b22
elena tarasova <nafanichka.ev@gmail.com>
2022-09-23 00:21:10 -0400

    Exersize1

---
+++ Exersize1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Write a number N: ");
+int N = int.Parse(Console.ReadLine());
+
+int a = N / 10000 % 10;
+int b = N % 10;
+int c = N / 1000 % 10;
+int d = N / 10 % 10;
+
+if ((a == b) && (c == d))
+{
+    Console.WriteLine("Your number is polindrome");
+}
+else
+{
+    Console.WriteLine("Sorry, your number is NOT polindrome");
+}
